b555a6f1dc1e8b5b625643d31d2d6c8bcee52bc2
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-04-22 14:27:50 +0400

    Инфраструктура серверной части

---
+++ MixerReportsServer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MixerReportsServer.ViewModels.Base;
+
+namespace MixerReportsServer.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Свойства
+
+        private string _Title = "Заливочные отчеты - Сервер";
+
+        /// <summary> Заголовок приложения </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
---
+++ MixerReportsServer/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MixerReportsServer.ViewModels
+{
+    class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowViewModel => App.Services
+            .GetRequiredService<MainWindowViewModel>();
+    }
+}
