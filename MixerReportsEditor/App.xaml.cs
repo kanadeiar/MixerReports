d7443d8677ac27b6218b4395acca1f5ca9190bdf
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-05-03 21:30:07 +0400

    Инфраструктура приложения +

--- MixerReportsEditor/App.xaml.cs
+++ MixerReportsEditor/App.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MixerReportsEditor.ViewModel;
 
 namespace MixerReportsEditor
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+    /// <summary> Interaction logic for App.xaml </summary>
     public partial class App : Application
     {
+        private static IServiceProvider __Services;
+        private static IServiceCollection GetServices()
+        {
+            var services = new ServiceCollection();
+            InitializeServices(services);
+            return services;
+        }
+        /// <summary> Сервисы </summary>
+        public static IServiceProvider Services => __Services ??= GetServices().BuildServiceProvider();
+        private static void InitializeServices(IServiceCollection services)
+        {
+            services.AddScoped<MainWindowViewModel>();
+        }
     }
 }
---
+++ MixerReportsEditor/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using MixerReportsEditor.Commands;
+
+namespace MixerReportsEditor.ViewModel
+{
+    class MainWindowViewModel : Base.ViewModel
+    {
+        #region Data
+
+        
+
+        #endregion
+
+        #region Properties
+
+
+
+
+
+        #region Supports
+
+        private string _Title = "Заливочные отчеты - Редактирование данных";
+
+        /// <summary> Заголовок приложения </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        #region Commands
+
+
+
+
+
+        #region Supports
+
+        private ICommand _CloseApplicationCommand;
+
+        /// <summary> Закрыть приложение </summary>
+        public ICommand CloseApplicationCommand => _CloseApplicationCommand ??=
+            new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
